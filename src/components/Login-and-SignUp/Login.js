d7503406e25dbc48917160b84007c75f06a74a16
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-06 20:30:22 +0530

    commit

---
+++ src/components/Login-and-SignUp/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Form, Button, Card } from 'react-bootstrap';
+import Heading from './Heading';
+import Email from './Email';
+import ORseperator from './ORseperator';
+import ContinueWith from './ContinueWith';
+import { FaGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
+export default function Login() {
+    const LoginText = `Welcome Back`;
+    return (
+        <Card>
+            <Heading heading={LoginText}></Heading>
+            <Email></Email>
+            <ORseperator></ORseperator>
+            <ContinueWith icon={FaGoogle} method={`Gooogle`}></ContinueWith>
+            <ContinueWith icon={faFacebook} method={`Facebook`}></ContinueWith>
+        </Card>
+    )
+}
