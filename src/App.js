704d4e5d449dc50f45514eafda85581e246843b3
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-06 21:54:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../src/components/Login-and-SignUp/Login";
-
+import './App.css'
 
 function App() {
   return (
