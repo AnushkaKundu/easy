f09ad91f0fdbd3286beefa510811d5c35103c954
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-07 17:09:12 +0530

    acommit

--- src/components/Login-and-SignUp/Email.js
+++ src/components/Login-and-SignUp/Email.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Email.css";
-const InputBox = () => {
+import { Form } from "react-bootstrap";
+export default function Email() {
     return (
-      <div className="input-box">
-        <input type="text" id="email-input" placeholder=" " required />
-        <label htmlFor="email-input">Email</label>
-      </div>
-    );
-  };
-  
-  export default InputBox;
+        <>
+            <div class="input-box">
+            <input type="text" class="input-field" id="email" autocomplete="off" required/>
+            <label for="email">Email</label>
+            </div>
+        </>
+    )
+}
