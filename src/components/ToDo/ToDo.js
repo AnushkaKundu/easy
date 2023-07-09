7e2fdd2369659ed1342b814324265f12494bc775
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-09 11:31:56 +0530

    commit

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,6 +1,11 @@
 import React from "react";
+import {Card} from 'react-bootstrap';
 import "./ToDo.css";
 
 export default function ToDo() {
-    
+    return(
+        <Card>
+            
+        </Card>
+    );
 }
