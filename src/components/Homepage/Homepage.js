3465f2b3c117b973016573aee540d32f7b0bd394
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-13 20:24:57 +0530

    commit

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
 import "./Homepage.css";
 import Navbar from "../Navbar/Navbar";
 import Heading from "../Login-and-SignUp/Heading";
 
 export default function Homepage({ toggleTheme }) {
-    const location = useLocation();
-    const { username } = location.state;
-
     return (
         <>
             <Navbar hb={true} toggleTheme={toggleTheme} />
             <div className="homepage">
                 <div className="hi"></div>
-                <Heading heading={`Good Morning, ${username}`} />
+                <Heading heading={`Good Morning`} />
             </div>
         </>
     );
