a8d983cf410084871167b64b288df8cdf00a0fb7
Anushka Kundu <Anu.kundu03@gmail.com>
2023-07-17 13:31:32 +0530

    commit

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Card } from 'react-bootstrap';
 import Navbar from "../Navbar/Navbar";
@@ -14,6 +14,28 @@ import 'firebase/compat/database';
 export default function ToDo({ toggleTheme }) {
     const location = useLocation();
     const encodedEmail = location?.state?.encodedEmail;
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const databaseRef = firebase.database().ref("users");
+        const userRef = databaseRef.child(encodedEmail);
+        const todosRef = userRef.child("todos");
+
+        todosRef.on("value", (snapshot) => {
+            const todosData = snapshot.val();
+            if (todosData) {
+                const todosArray = Object.values(todosData);
+                setTodos(todosArray);
+            } else {
+                setTodos([]);
+            }
+        });
+
+        return () => {
+            todosRef.off("value");
+        };
+    }, [encodedEmail]);
 
     const handleAddTodo = (todoValue) => {
         const databaseRef = firebase.database().ref("users");
@@ -31,9 +53,15 @@ export default function ToDo({ toggleTheme }) {
                 <Card>
                     <Heading heading={`To-Do`} />
                     <AddTodo handleAddTodo={handleAddTodo} />
-                    <UnitTodo content="jj" colour="light" />
-                    <UnitTodo content="jj" colour="medium" />
-                    <UnitTodo content="jj" colour="dark" />
+                    {todos.map((todo, index) => (
+                        <UnitTodo
+                            key={index}
+                            content={todo}
+                            colour={index % 3 === 0 ? "light" : index % 3 === 1 ? "medium" : "dark"}
+                        />
+                    ))}
+                    <Heading heading={`Completed ToDos`} className="completed-heading" />
+                    <hr className="completed" />
                 </Card>
             </div>
         </>
